a8a7e6fa6825eeeeecf961ada02fe5b4373099ec
alexpott <alexpott@157725.no-reply.drupal.org>
2019-03-13 17:23:34 +0000

    Issue #3039805 by alexpott, webadpro, heddn: Use a custom id_map plugin and provide a computed field

--- tests/src/Functional/ProductCategorySyncTest.php
+++ tests/src/Functional/ProductCategorySyncTest.php
@@ -6,6 +6,7 @@ use Drupal\Core\Url;
 use Drupal\migrate\MigrateExecutable;
 use Drupal\migrate\MigrateMessageInterface;
 use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\taxonomy\Entity\Term;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -49,6 +50,23 @@ class ProductCategorySyncTest extends BrowserTestBase implements MigrateMessageI
       ->getStorage('taxonomy_term')
       ->loadByProperties(['vid' => 'bigcommerce_product_category']);
     $this->assertCount(6, $terms);
+
+    // Test the bigcommerce_id field.
+    $this->assertSame(20, Term::load(1)->bigcommerce_id->value);
+    $this->assertSame(18, Term::load(2)->get('bigcommerce_id')->value);
+    $this->assertSame(19, Term::load(3)->get('bigcommerce_id')->value);
+    $this->assertSame(21, Term::load(4)->get('bigcommerce_id')->value);
+    $this->assertSame(22, Term::load(5)->get('bigcommerce_id')->value);
+    $this->assertSame(23, Term::load(6)->get('bigcommerce_id')->value);
+
+    // Test terms not created via syncing are not broken.
+    $term = Term::create([
+      'vid' => 'bigcommerce_product_category',
+      'name' => 'a fake test',
+    ]);
+    $this->assertNull($term->get('bigcommerce_id')->value);
+    $term->save();
+    $this->assertNull(Term::load(7)->get('bigcommerce_id')->value);
   }
 
   /**
