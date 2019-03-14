8829b416337b79e6a9aa3a7775a86d3e5ee4750f
webadpro <webadpro@708024.no-reply.drupal.org>
2019-03-14 22:15:38 +0000

    Issue #3036577 by webadpro, alexpott: Sync Product Options (Product Attributes)

---
+++ src/Plugin/migrate/source/ProductOptionValue.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\bigcommerce\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+
+/**
+ * Gets all Product Options from BigCommerce API.
+ *
+ * @MigrateSource(
+ *   id = "bigcommerce_product_option_value"
+ * )
+ */
+class ProductOptionValue extends ProductOption {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getYield(array $params) {
+    $total_pages = 1;
+    while ($params['page'] < $total_pages) {
+      $params['page']++;
+
+      $response = $this->getSourceResponse($params);
+      foreach ($response->getData() as $option) {
+        foreach ($option->getOptionValues() as $value) {
+          $data = $value->get();
+          $data['attribute_name'] = $option->getName();
+          $data['attribute_type'] = $option->getType();
+          yield $data;
+        }
+      }
+
+      if ($params['page'] === 1) {
+        $total_pages = $response->getMeta()->getPagination()->getTotalPages();
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $migration = $this->migration;
+    $process = $migration->get('process');
+
+    // Clear fields process so we do not map fields which aren't available.
+    foreach (array_keys($process) as $field_name) {
+      if (strpos($field_name, "field_product_attribute") !== FALSE) {
+        unset($process[$field_name]);
+      }
+    }
+
+    // Look up if this row has any fields to fill out.
+    $fields = $this->getOptionFields($row->getSourceProperty('attribute_type'));
+    if (!empty($fields)) {
+      foreach ($fields as $field) {
+        if ($row->hasSourceProperty($field['source'])) {
+          if (!empty($field['process'])) {
+            $process[$field['field_name']] = $field['process'];
+          }
+          else {
+            $process[$field['field_name']] = $field['source'];
+          }
+        }
+      }
+      $migration->set('process', $process);
+    }
+    return parent::prepareRow($row);
+  }
+
+}
